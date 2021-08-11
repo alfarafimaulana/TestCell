357c39163909ce4448a93bf723548eb8ba3c3637
alfarafimaulana <alfarafi.maulana.am@gmail.com>
2021-08-11 09:32:29 +0800

    new ui

--- app/src/main/java/com/example/testcell/MainActivity.kt
+++ app/src/main/java/com/example/testcell/MainActivity.kt
@@ -4,14 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-
+        timeKnown()
 
         val test : ImageView = findViewById(R.id.testImage)
         test.setOnClickListener{
@@ -25,10 +28,27 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val bug : FloatingActionButton = findViewById(R.id.bugFloating)
+        val bug : ImageView = findViewById(R.id.bugFloating)
         bug.setOnClickListener{
             val intent = Intent(this,bugReportAct::class.java)
             startActivity(intent)
         }
     }
+
+    fun timeKnown(){
+        var timeOfDay = Calendar.HOUR_OF_DAY
+        val timep : TextView = findViewById(R.id.waktuText)
+        if(timeOfDay >= 0 && timeOfDay < 12){
+            timep.text = "Selamat Pagi"
+        }else if(timeOfDay >= 12 && timeOfDay < 16){
+            timep.text = "Selamat Siang"
+        }else if(timeOfDay >= 16 && timeOfDay < 21){
+            timep.text = "Selamat Sore"
+        }else if(timeOfDay >= 21 && timeOfDay < 24){
+            timep.text = "Selamat Malam"
+        }
+    }
+
+
+
 }
