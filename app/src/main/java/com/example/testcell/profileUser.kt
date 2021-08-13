3bbfa25967f7acf9d24ccc354b2445b038257aa4
alfarafimaulana <alfarafi.maulana.am@gmail.com>
2021-08-13 14:48:58 +0800

    user image profileUser.kt

--- app/src/main/java/com/example/testcell/profileUser.kt
+++ app/src/main/java/com/example/testcell/profileUser.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 
 class profileUser : AppCompatActivity() {
@@ -13,6 +15,10 @@ class profileUser : AppCompatActivity() {
         setContentView(R.layout.activity_profile_user)
         mAuth = FirebaseAuth.getInstance()
         val currentUser = mAuth.currentUser
+
+        val gambarUserProfile : ImageView = findViewById(R.id.gambarUserProfile)
+        Glide.with(this).load(currentUser?.photoUrl).into(gambarUserProfile)
+
 
         val signOut : Button = findViewById(R.id.signOut)
         signOut.setOnClickListener{
